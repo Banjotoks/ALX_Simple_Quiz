b2483fd7d42bfaa0d6f920c8cf53f7655e5569d5
Banjotoks <banjotokunboh@gmail.com>
2024-07-17 22:58:35 +0100

    Create a simple calculator

---
+++ practice.js
@@ -0,0 +1,10 @@
+//Arrays
+let fruits = ["apple", "orange", "banana"];
+
+let numOfFruits = fruits.length;
+
+console.log (numOfFruits); 
+console.log (fruits[0]);
+console.log (fruits[1]);
+console.log (fruits[2]);
+console.log (fruits[3]);
