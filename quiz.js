a151abd30d0aa8dec59fde67f5faf38127c149f8
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-07-17 23:43:33 +0100

    Create calculator

---
+++ quiz.js
@@ -0,0 +1,17 @@
+// function declaration to check answer
+function checkAnswer() {
+    let correctAnswer = "4";
+    // retrieve users answer
+    let userAnswer = document.querySelector("input[name=quiz]:checked").value;
+    // getting users answer
+    if (userAnswer === correctAnswer) {
+        document.getElementById("feedback").textContent = "Correct! well done" 
+    }
+    else {
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!"
+
+    }
+    
+}
+//button for function execution
+document.getElementById("submit-answer").addEventListener('click',checkAnswer)
